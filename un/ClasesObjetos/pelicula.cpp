eedb9a6cd6ab0708a76e4fb24537b6abe08a5391
Jair Mendoza Flores <jmmendozaf@unjbg.edu.pe>
2026-05-26 23:20:29 -0500

    Ejercicios de Clases y Objetos

---
+++ un/ClasesObjetos/pelicula.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class clasePelicula{
+    public:
+        char titulo[20];
+        int duracion;
+        float precio;
+
+        void registrar();
+        void imprimirInfo();
+};
+
+int main(){
+
+    return 0;
+}
