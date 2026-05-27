7cb24471afe3059036d86dc1fbeac56893a7451b
Jair Mendoza Flores <jmmendozaf@unjbg.edu.pe>
2026-05-26 23:35:26 -0500

    Creación de objeto

--- un/ClasesObjetos/pelicula.cpp
+++ un/ClasesObjetos/pelicula.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 class clasePelicula{
@@ -11,7 +12,29 @@ class clasePelicula{
         void imprimirInfo();
 };
 
+void clasePelicula::registrar(){
+    cout<<"= REGISTRAR =\n";
+    cout<<"Título: "; fflush(stdin); gets(titulo);
+    cout<<"Duración (min): "; cin>>duracion;
+    cout<<"Precio (S/): "; cin>>precio;
+    cout<<endl;
+}
+
+void clasePelicula::imprimirInfo(){
+    cout<<"= DATOS REGISTRADOS =\n";
+    cout<<"Título: "<<titulo<<endl;
+    cout<<"Duración (min): "<<duracion<<endl;
+    cout<<"Precio (S/): "<<precio<<endl;
+    cout<<endl;
+}
+
 int main(){
+    SetConsoleOutputCP(CP_UTF8);
+
+    clasePelicula titanic;
+    titanic.registrar();
+    cout<<"Título: "<<titanic.titulo<<endl<<endl;
+    titanic.imprimirInfo();
 
     return 0;
 }
