be66d8ced5954654d52ab3edf654eb2581ce9382
Jair Mendoza Flores <jmmendozaf@unjbg.edu.pe>
2026-05-31 22:31:20 -0500

    Atributos de cada cada clase

--- un/HerenciaSimple/figura.cpp
+++ un/HerenciaSimple/figura.cpp
@@ -3,27 +3,47 @@ using namespace std;
 
 class figura{
     protected:
+        string color;
     public:
+        figura(string c);
 };
+
+figura::figura(string c){
+    color=c;
+}
 /*
 ---------------------------------------------------------------------
 */
 class circulo:public figura{
     private:
+        float radio;
     public:
+        circulo(string c, float r);
+        void calcularArea();
+        void calcularOerimetro();
 };
 /*
 ---------------------------------------------------------------------
 */
 class rectangulo:public figura{
     private:
+        float base;
+        float altura;
     public:
+        rectangulo(string c, float b, float a);
+        void calcularArea();
 };
 /*
 ---------------------------------------------------------------------
 */
 class triangulo:public figura{
-
+    private:
+        float base;
+        float altura;
+        string tipo;
+    public:
+        triangulo(string c, float b, float a, string t);
+        void calcularArea();
 };
 /*
 ---------------------------------------------------------------------
