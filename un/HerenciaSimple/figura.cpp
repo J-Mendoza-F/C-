83e745998a1dd0b47348a8f9a658db5eed3a614b
Jair Mendoza Flores <jmmendozaf@unjbg.edu.pe>
2026-05-31 22:24:48 -0500

    Base de ejercicio figura.cpp

---
+++ un/HerenciaSimple/figura.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class figura{
+    protected:
+    public:
+};
+/*
+---------------------------------------------------------------------
+*/
+class circulo:public figura{
+    private:
+    public:
+};
+/*
+---------------------------------------------------------------------
+*/
+class rectangulo:public figura{
+    private:
+    public:
+};
+/*
+---------------------------------------------------------------------
+*/
+class triangulo:public figura{
+
+};
+/*
+---------------------------------------------------------------------
+*/
+int main(){
+
+
+    return 0;
+}
